ec2f2e6903228e95fb48f534aed8a1584451d1c8
Gabriel <gabrielfernandesfg@hotmail.com>
2021-09-07 20:47:35 -0300

    refactor: db config service adbstraction

--- src/cli/loader.ts
+++ src/cli/loader.ts
@@ -1,23 +1,13 @@
 require('dotenv').config({ path: `.env` });
 
 import { ConfigService } from '@nestjs/config';
+import DatabaseConfigService from '../modules/config/services/DatabaseConfigService';
 
 const configService = new ConfigService();
+const databaseConfigService = new DatabaseConfigService(configService);
 
 export default {
-    ...{
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: ['dist/**/*Entity.js'],
-        synchronize: false,
-        cli: {
-            migrationsDir: 'src/migrations',
-        },
-    },
+    ...databaseConfigService.config,
     ...{
         migrations: ['src/migrations/**/*.ts'],
     },
